68e44e273f065c50a25d5bc9e041c094a10a14c2
1143863170 <brayan.echavarria@segurosbolivar.com>
2024-06-21 10:04:13 -0500

    first-commit

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = merge(
-    var.tags,
-    { Name = "prueba-vpc" },
-  )
-}
-
 # Create network y VPC
 /* module "network" {
   source             = "./modules/network"
