b9b8f3a2275266951e8b673adef2db57004ef7af
1143863170 <brayan.echavarria@segurosbolivar.com>
2024-06-23 16:00:18 -0500

    Cambio version python Lambda

--- main.tf
+++ main.tf
@@ -123,13 +123,11 @@ module "lambda_sns_check_lambda" {
           "Version": "2012-10-17",
           "Statement": [
             {
-              Action = [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-              ],
-              Effect   = "Allow",
-              Resource = "arn:aws:logs:*:*:*"
+              "Effect": "Allow",
+              "Action": [
+                "logs:*",
+            ],
+              "Resource": "*"
             },
             {
               Action = [
