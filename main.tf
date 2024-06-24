86c15b289a0e4c9ee9506245d9e694d8718ffa29
1143863170 <brayan.echavarria@segurosbolivar.com>
2024-06-24 08:17:01 -0500

    Cambio Lambda

--- main.tf
+++ main.tf
@@ -147,6 +147,17 @@ module "lambda_sns_check_lambda" {
               ],
               Effect   = "Allow",
               Resource = aws_sns_topic.email_notifications.arn
+            },
+            {
+              Effect = "Allow",
+              Action = [
+                "s3:GetObject",
+                "s3:ListBucket"
+              ],
+              Resource = [
+                "${aws_s3_bucket.bucket_test_data.arn}",
+                "${aws_s3_bucket.bucket_test_data.arn}/*"
+              ]
             }
           ]
         }
